2a01d74f1372a5da7b7d7c85eee0925c305cd0a4
Curious-DevlOper <elham.bolandi@gmai.com>
2023-10-08 11:47:15 +0200

    Added multiple project folders

---
+++ product-landing-page/practice.js
@@ -0,0 +1,15 @@
+// const myArray = [["John", 23], ["dog", 3]];
+
+// Add elements to the beginning of the array
+// myArray.shift();       // Adds 1 to the beginning
+// myArray.unshift(["Paul", 35]);   
+
+const dogs = {
+    Fido: "Mutt",
+    Hunter: "Doberman",
+    Snoopie: "Beagle"
+  };
+  
+  const myDog = "Hunter";
+  const myBreed = dogs[myDog];
+  console.log(myBreed);
